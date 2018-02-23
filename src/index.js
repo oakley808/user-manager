116dae5137dea50b38e6bf2b4c251d3b404d065b
Michael Oakley <oakley808@gmail.com>
2018-02-23 09:18:43 -0700

    Add redux, router and friends. Setup for higher level integrations.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
+/**
+ * index.js
+ * This is the entry file for the application
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Import root app
+import App from 'containers/App';
+
+import configureStore from './configureStore';
+
+// Import CSS reset and global styles
+import 'sanitize.css/sanitize.css';
+import './index.css';
+
+
+// Create redux store with history
+const initialState = {};
+const history = createHistory();
+const store = configureStore(initialState, history);
+const MOUNT_NODE = document.getElementById('root');
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  MOUNT_NODE
+);
 registerServiceWorker();
